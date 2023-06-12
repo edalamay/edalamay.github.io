26cfb93adaef26bc5854b2a177d1037a2225c43f
Cameron Harbrech <cameron@grossbauer.com>
2023-06-12 14:14:43 -0500

    initial commit, cloning a basic gulp repo

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+// Example gulp 4 file
+// https://gist.github.com/jeromecoupe/0b807b0c1050647eb340360902c3203a
+
+"use strict";
+
+const { src, dest, watch, series } = require('gulp');
+const gutil = require("gulp-util");
+const sass = require('gulp-sass')(require('sass'));
+const browsersync = require("browser-sync").create();
+const autoprefixer = require("gulp-autoprefixer");
+const terser = require('gulp-terser');
+const rename = require("gulp-rename");
+const postcss = require('gulp-postcss');
+const cssnano = require("cssnano");
+const concat = require('gulp-concat');
+
+
+// Build app.css file
+function css() {
+	return src("resources/scss/app.scss")
+		.pipe(sass({
+			debugInfo: true,
+			lineNumbers: true
+		}).on("error", sass.logError))
+		.pipe(autoprefixer("last 4 version"))
+		.pipe(dest("public/css"))
+		.pipe(postcss([cssnano()]))
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(dest("public/css"))
+		.pipe(browsersync.reload({ stream: true }));
+};
+// build third-party CSS files that can't be compiled through PostCSS
+function libraries() {
+	return src([
+			"node_modules/bootstrap/dist/css/bootstrap.min.css"
+		])
+		.pipe(dest("public/css"));
+}
+// Build JS
+function js() {
+	return src([
+			"node_modules/bootstrap/dist/js/bootstrap.min.js",
+			"resources/js/app.js"
+		])
+		.pipe(concat("app.js"))
+		.pipe(dest("public/js"))
+		.pipe(terser())
+		.on("error", function(err) {
+			gutil.log(gutil.colors.red("[Error]"), err.toString());
+		})
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(dest("public/js"))
+		.pipe(browsersync.reload({ stream: true, once: true }));
+};
+// Run Browsersync through port 3000 for local testing with live reload
+function browsersyncServe(cb){
+	browsersync.init({
+	server: {
+		baseDir: 'public'
+	}
+	});
+	cb();
+}
+function browsersyncReload(cb){
+	browsersync.reload();
+	cb();
+}
+
+// Watch for file changes within specified directories
+function watchTask(){
+	watch('public/*.html', browsersyncReload);
+	watch('resources/scss/*.scss', series(css, browsersyncReload));
+	watch('resources/js/*.js', series(js, browsersyncReload));
+}
+
+// Run our tasks
+exports.default = series(
+	css,
+	libraries,
+	js,
+	browsersyncServe,
+	watchTask
+);
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Gulp 4 Boilerplate
+
+A boilerplate project directory for static websites, utilizing Gulp 4 for asset compiling. Utilizes Bootstrap 5 for easy & quick templating.
+
+## Installation
+
+Open the directory in terminal and run:
+
+```bash
+npm i
+```
+
+## Usage
+Simply run gulp to compile your assets and deploy a localhost environment for testing:
+```bash
+gulp
+```
+
+## Contributing
+Feel free to make any adjustments or edits to help improve this template for future use.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
