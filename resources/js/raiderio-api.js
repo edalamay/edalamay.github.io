8603e265c10de1c3a5b3d629c98ddcf4f3f5beac
edalamay <cameron.harbrecht+commit@gmail.com>
2023-08-04 09:12:38 -0500

    Disabling the API fetch for CE kills - dynamically grabbing data that is ultimately static is a waste of time & effort. Was neat tho

--- resources/js/raiderio-api.js
+++ resources/js/raiderio-api.js
@@ -139,26 +139,24 @@ function updateProgData() {
 
 // Grabs meta data for CE kill blocks
 //===========================================
-raids.forEach((raid, index) => {
-	const boss = bosses[index];
-	const bossRanking = `https://raider.io/api/v1/raiding/boss-rankings?raid=${raid}&boss=${boss}&difficulty=${difficulty}&region=${region}&realm=${realm}`;
-	fetch(bossRanking)
-	.then((response) => response.json())
-	.then((data) => {
-		let _data = data.bossRankings;
-		_data.forEach((entry) => {
-			if (entry.guild.name === 'Commit') {
-				let rankElm = document.querySelector(`#${boss} .rank`),
-					dateElm = document.querySelector(`#${boss} .date`),
-					killDate = new Date(entry.encountersDefeated[0].firstDefeated).toLocaleDateString('en-us', {month:"long",day:"numeric",year:"numeric"});
-				rankElm.innerHTML = 'US '+entry.regionRank;
-				dateElm.innerHTML = killDate;
-				// console.log(boss+' Kill Date: '+killDate);
-			}
-		});
-	});
-	// console.log(raid, boss);
-});
+// raids.forEach((raid, index) => {
+// 	const boss = bosses[index];
+// 	const bossRanking = `https://raider.io/api/v1/raiding/boss-rankings?raid=${raid}&boss=${boss}&difficulty=${difficulty}&region=${region}&realm=${realm}`;
+// 	fetch(bossRanking)
+// 	.then((response) => response.json())
+// 	.then((data) => {
+// 		let _data = data.bossRankings;
+// 		_data.forEach((entry) => {
+// 			if (entry.guild.name === 'Commit') {
+// 				let rankElm = document.querySelector(`#${boss} .rank`),
+// 					dateElm = document.querySelector(`#${boss} .date`),
+// 					killDate = new Date(entry.encountersDefeated[0].firstDefeated).toLocaleDateString('en-us', {month:"long",day:"numeric",year:"numeric"});
+// 				rankElm.innerHTML = 'US '+entry.regionRank;
+// 				dateElm.innerHTML = killDate;
+// 			}
+// 		});
+// 	});
+// });
 
 
 
