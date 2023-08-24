37b2e1367aad2052c2c80287bbd0600de1084974
edalamay <cameron.harbrecht+commit@gmail.com>
2023-08-23 23:20:29 -0500

    Drop the time diff to 30min

--- resources/js/warcraftlogs-api.js
+++ resources/js/warcraftlogs-api.js
@@ -101,7 +101,7 @@ function unixRounding(value) {
 	return value;
 }
 function timeCompare(time1,time2) {
-	let unixHours = 1 * 60 * 60; // 1 hour in seconds
+	let unixHours = 0.5 * 60 * 60; // 1 hour in seconds
 	if ( (time1 - time2) < unixHours ) {
 		return true; // reports are duplicates
 	} else {
