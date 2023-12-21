c8ef1718a7f683d7c7dfc4041877412a19f42294
edalamay <cameron.harbrecht+commit@gmail.com>
2023-12-21 00:48:38 -0600

    adding a streamer

--- resources/js/app.js
+++ resources/js/app.js
@@ -210,7 +210,7 @@
 
 	async function getStreams() {
 		// const users = ['soap','adeenthequeen','veraneka','eyelashTV','warcraft','naguura','sco','naowh','gingitv','hazelnuttygames','cdewx','fragnance']; //testing array
-		const users = ['samthepackleader','bowett','edalamay','asherrthered','aka_vinny','redpandacake']; // live array
+		const users = ['samthepackleader','bowett','edalamay','asherrthered','aka_vinny','redpandacake','Maximino_V']; // live array
 		const endpoint = "https://api.twitch.tv/helix/streams?user_login="+users.join('&user_login=');
 
 		let authorizationObject = await getTwitchAuthorization();
